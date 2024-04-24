c9861e46290f7ca3fa943fa972b0c65e25a47917
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2024-04-24 20:28:45 -0300

    Added genre to browser router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import LoginSignupPage from "./pages/LoginSignupPage";
 import Page404 from "./pages/Page404";
 import BrowserPage from "./pages/BrowserPage";
 import NavBar from "./components/NavBar";
+import GenresPage from "./pages/GenresPage";
 
 function App() {
 	return (
 		<div className="bg-[#FAF9F6] min-h-svh text-slate-700 dark:bg-slate-800 dark:text-slate-300 flex">
-			<BrowserRouter>
+			<BrowserRouter basename="/">
 				<NavBar></NavBar>
 				<Routes>
 					<Route path="/" element={<BrowserPage />}></Route>
+					<Route path="/genres" element={<GenresPage />}></Route>
 					<Route path="/signup" element={<LoginSignupPage />}></Route>
 					<Route path="/login" element={<LoginSignupPage />}></Route>
 					<Route path="/*" element={<Page404 />}></Route>
